6b9c61ff03d7f4bf9454fc865f70eedf20e1cb00
Tim Crooker <timothycrooker@gmail.com>
2021-10-30 14:05:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# sao-generator
+
+A generator for scaffolding out a SAO generator.
+
+## Features
+
+- Bare minimal generator
+- Unit test with [AVA](https://ava.li)
+
+## Usage
+
+Install [SAO](https://github.com/saojs/sao) first.
+
+```bash
+yarn global add sao
+# or
+npm i -g sao
+```
+
+### From npm
+
+```bash
+sao generator sao-name
+```
+
+### From repo
+
+```bash
+sao saojs/sao-generator sao-name
+```
+
+## License
+
+MIT.
---
+++ generator.ts
@@ -0,0 +1,67 @@
+import {SAO, GeneratorConfig} from 'projenerator'
+
+export default { 
+	prompts() {
+    return [
+      {
+        name: 'name',
+				type: 'input',
+        message: 'What is the name of the new generator (must be sao-*)',
+        default: `sao-${this.outDir.replace(/^sao-/, '')}`,
+        filter: val => val.toLowerCase(),
+        validate: val => val.startsWith('sao-')
+      },
+      {
+        name: 'description',
+				type: 'input',
+        message: 'How would you describe the new template',
+        default: `my awesome SAO generator`
+      },
+      {
+        name: 'username',
+				type: 'input',
+        message: 'What is your GitHub username',
+        default: this.gitUser.username || this.gitUser.name,
+        filter: val => val.toLowerCase(),
+        store: true
+      },
+      {
+        name: 'email',
+				type: 'input',
+        message: 'What is your email?',
+        default: this.gitUser.email,
+        store: true
+      },
+      {
+        name: 'website',
+				type: 'input',
+        message: 'The URL of your website',
+        default(data) {
+          return `github.com/${data.answers.username}`
+        },
+        store: true
+      }
+    ]
+  },
+  actions: [
+    {
+      type: 'add',
+      files: '**',
+      transformExclude: 'template/**'
+    },
+    {
+      type: 'move',
+      patterns: {
+        gitignore: '.gitignore',
+        // If we use `package.json` directly
+        // Then `template` folder will be treated as a package too, which isn't safe
+        '_package.json': 'package.json'
+      }
+    }
+  ],
+  async completed() {
+    this.gitInit()
+    await this.npmInstall()
+    this.showProjectTips()
+  }
+} as GeneratorConfig
